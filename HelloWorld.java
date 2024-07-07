cd6c9aa4f38db08b923d03e83097dea7fc33a2fd
ReLuckyLucy <1257716130@qq.com>
2024-07-07 21:36:00 +0800

    1

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,18 @@
+/**
+ * 主类，用于执行简单的 "Hello World" 程序。
+ */
 public class HelloWorld {
+    
+    /**
+     * 主方法，程序的入口点。Java 应用程序通常从这里开始执行。
+     *
+     * @param args 传入命令行参数（通常是空数组）
+     */
     public static void main(String[] args) {
+        
+        /**
+         * 打印 "Hello World" 到控制台。
+         */
         System.out.println("Hello World");
     }
 }
