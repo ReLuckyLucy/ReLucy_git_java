419e296474858f6ea72681e6aec6347c3546e28b
ReLuckyLucy <1257716130@qq.com>
2024-08-28 20:33:29 +0800

    commit

---
+++ PrimitiveTypeTest/RunoobTest.java
@@ -0,0 +1,25 @@
+public class RunoobTest {
+    // 成员变量
+    private int instanceVar;
+    // 静态变量
+    private static int staticVar;
+    
+    public void method(int paramVar) {
+        // 局部变量
+        int localVar = 10;
+        
+        // 使用变量
+        instanceVar = localVar;
+        staticVar = paramVar;
+        
+        System.out.println("成员变量: " + instanceVar);
+        System.out.println("静态变量: " + staticVar);
+        System.out.println("参数变量: " + paramVar);
+        System.out.println("局部变量: " + localVar);
+    }
+    
+    public static void main(String[] args) {
+        RunoobTest v = new RunoobTest();
+        v.method(20);
+    }
+}
