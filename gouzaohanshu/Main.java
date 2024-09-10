aa5ca7a48a192f9138a64138e1ae2f4b4cfdbf1b
ReLuckyLucy <1257716130@qq.com>
2024-09-10 10:48:09 +0800

    构造函数

---
+++ gouzaohanshu/Main.java
@@ -0,0 +1,9 @@
+package gouzaohanshu;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("Alice", 30);
+        System.out.println(person.getName()); // 输出 "Alice"
+        System.out.println(person.getAge());  // 输出 30
+    }
+}
